e3c6430df271cddad63d5425d7ce807cd067a12a
dylan <nais95100@icloud.com>
2018-04-30 00:20:33 +0200

    added Chest class + chestAppear() function + discoverWeaponBonusOrMagicianBonus() + resetWeaponsCharacter() + added dictionaries containing bonus weapons/heal.

--- Game_Project_3/Chest.swift
+++ Game_Project_3/Chest.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+
+class Chest {
+    let weaponsBonus: [WeaponsBonus: Int] = [.woodenArch: 14, .gun: 16, .uAV: Int(arc4random_uniform(3))+17, .c4: Int(arc4random_uniform(3))+19]
+    let magicianBonus: [MagicianBonus: Int] = [.plasters: 14, .medicalKit: 16, .smallPotion: Int(arc4random_uniform(3))+17, .BigPotion: Int(arc4random_uniform(3))+19]
+    
+    
+    func chestAppears(attackers: Characters) {
+        let indexWeaponsBonus: Int = Int(arc4random_uniform(UInt32(weaponsBonus.count)))
+        let indexMagicianBonus: Int = Int(arc4random_uniform(UInt32(magicianBonus.count)))
+        
+        let arrayWeaponsBonus = Array(weaponsBonus)[indexWeaponsBonus]
+        let arrayMagicianBonus = Array(magicianBonus)[indexMagicianBonus]
+        
+        if attackers.type != .magician {
+            //print message discovery of the chest
+            print(communication.chestAppears)
+            attackers.weapon = arrayWeaponsBonus.key.rawValue
+            attackers.weaponDamages = arrayWeaponsBonus.value
+            discoverWeaponBonusOrMagicianBonus(character: attackers)
+            
+        } else {
+            //print message discovery of the chest
+            print(communication.chestAppears)
+            attackers.weapon = arrayMagicianBonus.key.rawValue
+            attackers.weaponDamages = arrayMagicianBonus.value
+            discoverWeaponBonusOrMagicianBonus(character: attackers)
+        }
+    }
+    func discoverWeaponBonusOrMagicianBonus(character: Characters) {
+        if character.type != .magician {
+            print("En ouvrant le coffre \(character.name) à trouvé un \(character.weapon)\nDégats: \(character.weaponDamages)Pv\n")
+            print("\n\(character.name) s'équipe de la nouvelle arme le temps de l'attaque.\n")
+        } else {
+            print("En ouvrant le coffre \(character.name) a trouvé des \(character.weapon)\nSoin: \(character.healer!)Pv.\n")
+            print("\n\(character.name) s'équipe du nouvel equipement le temps de l'attaque.\n")
+        }
+    }
+    func resetWeaponsCharacter(character: Characters) {
+        if character.type == .colossus {
+            character.weaponDamages = Damages.colossus.rawValue
+        } else if character.type == .magician {
+            character.weaponDamages = Damages.magician.rawValue
+        } else if character.type == .colossus {
+            character.weaponDamages = Damages.colossus.rawValue
+        } else {
+            character.weaponDamages = Damages.gnome.rawValue
+        }
+    }
+}
+
