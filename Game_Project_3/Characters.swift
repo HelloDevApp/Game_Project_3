51dec7a4c2fab35b1541ae2a3c78dc311bb4ca45
dylan <nais95100@icloud.com>
2018-04-30 00:18:11 +0200

    adding capital letters for enumerations. added WeaponsBonus and MagicianBonus enumerations. added Chest class + chestAppear() function + discoverWeaponBonusOrMagicianBonus() + resetWeaponsCharacter() + added dictionaries containing bonus weapons/heal.

--- Game_Project_3/Characters.swift
+++ Game_Project_3/Characters.swift
@@ -8,42 +8,53 @@
 
 import Foundation
 
-enum type: String {
+enum Type: String {
     case soldier = "Combattant"
     case magician = "Mâge"
     case colossus = "Colosse"
     case gnome = "Nain"
 }
-enum weapons: String {
+enum Weapons: String {
     case soldier = "Épée"
     case magician = "Dague Soignante"
     case colossus = "Poing en Fer"
     case gnome = "Arbalète"
 }
-enum life: Int {
+enum Life: Int {
     case soldier = 100
     case magician = 50
     case colossus = 110
     case gnome = 18
 }
-enum damages: Int {
+enum Damages: Int {
     case soldier = 10
     case magician = 9
     case colossus = 7
     case gnome = 21
 }
-
+enum WeaponsBonus: String {
+    case woodenArch = "Arc-En-Bois"
+    case gun = "Pistolet"
+    case uAV = "Drone"
+    case c4 = "Kit Explosif"
+}
+enum MagicianBonus: String {
+    case plasters = "Pansements"
+    case medicalKit = "Kits Médical"
+    case smallPotion = "Petite Potions"
+    case BigPotion = "Grande Potions"
+}
 class Characters {
     var indexPosition: Int
     let idNumber: Int
-    var type: type
+    var type: Type
     var name: String
     var life: Int
-    var weaponDamages: damages
+    var weaponDamages: Damages.RawValue
     var healer: Int?
-    var weapon: weapons
+    var weapon: Weapons.RawValue
     
-    init(indexPosition: Int, idNumber: Int, type: type, name: String, life: Int, weaponDamages: damages, healer: Int?, weapon: weapons) {
+    init(indexPosition: Int, idNumber: Int, type: Type, name: String, life: Int, weaponDamages: Int, healer: Int?, weapon: String) {
         self.indexPosition = indexPosition
         self.idNumber = idNumber
         self.type = type
