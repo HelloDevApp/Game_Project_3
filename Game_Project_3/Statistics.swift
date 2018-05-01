ada2cdaacc16a01e2b61a1e25d575b6a3a2409ae
dylan <nais95100@icloud.com>
2018-05-01 13:49:07 +0200

    added the swift Statistics file and the class of the same name + the properties that compose it

---
+++ Game_Project_3/Statistics.swift
@@ -0,0 +1,16 @@
+//
+//  Statistics.swift
+//  Game_Project_3
+//
+//  Created by Mac Book Pro on 01/05/2018.
+//  Copyright © 2018 dylan. All rights reserved.
+//
+
+import Foundation
+
+class Statistics {
+    var numberAssault = [0,0,0]
+    var numberHeal = [0,0,0]
+    var totalWeaponDamages = [0,0,0]
+    var totalNumberChest = 0
+}
